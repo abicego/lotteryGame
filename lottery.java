0e45fc761dadd4fed74c3962c3506bd4644fbdc1
Angélica Bicego <59181068+abicego@users.noreply.github.com>
2021-09-19 21:47:03 -0300

    Update lottery.java

--- lottery.java
+++ lottery.java
@@ -6,17 +6,17 @@ public class lottery {
     public static void main(String[] args){
 
         Scanner numberScanner = new Scanner(System.in);
-        System.out.println("Informe um número de 1 a 10: ");
+        System.out.println("Number of 1 to 10: ");
         int userNum = numberScanner.nextInt();
         Random rnd= new Random();
         int winningNum = rnd.nextInt(10) + 1;
-        System.out.println("Seu número: " + userNum);
-        System.out.println("O Número vencedor é: " + winningNum);
+        System.out.println("Your numbrer: " + userNum);
+        System.out.println("The winner number: " + winningNum);
 
         if (userNum == winningNum){
-            System.out.println("Parabéns, você venceu!");
+            System.out.println("Congratulations, you win!");
         } else{
-            System.out.println("Que pena, tente de novo!");
+            System.out.println("Oh no, try again to be a winner!");
         }
 
     }
